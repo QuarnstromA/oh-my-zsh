620bced07bc6349895727bc11de366af97c3fb4e
Anthony Quarnstrom <quarnstroma@gmail.com>
2020-01-29 21:28:20 -0700

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -34,7 +34,7 @@ set -e
 # Default settings
 ZSH=${ZSH:-~/.oh-my-zsh}
 REPO=${REPO:-quarnstroma/ohmyzsh}
-REMOTE=${REMOTE:-https://github.com/${REPO}.git}
+REMOTE=${REMOTE:-git@github.com/${REPO}.git}
 BRANCH=${BRANCH:-master}
 
 # Other options
